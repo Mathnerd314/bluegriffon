b6b9c5bf67d33e39c4efbe79393c4cd45f90ab9c
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2008-10-02 10:14:01 +0000

    table insertion dialogs   html attrs OK   inline styles OK   CSS rules OK   reuse existing style rules TBD
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@22 c25cacc5-b657-0410-8d04-e2f7359d2943

--- base/content/bluegriffon/dialogs/insertTable.js
+++ base/content/bluegriffon/dialogs/insertTable.js
@@ -15,6 +15,7 @@ function Startup()
 
   GetUIElements();
   SetWidthTextBoxMax(null);
+  gDialog.cssToggler.init();
 }
 
 function SetWidthTextBoxMax(aElt)
@@ -82,6 +83,33 @@ function ShowSize()
   gDialog.sizeLabel.value = rows + " x " + columns;
 }
 
+function CreateRowsAndCells(aTbody, aAttributes)
+{
+  // Create necessary rows and cells for the table
+  for (var i = 0; i < gDialog.rowsInput.value; i++)
+  {
+    var newRow = gActiveEditor.document.createElement("tr");
+    if (newRow)
+    {
+      aTbody.appendChild(newRow);
+      for (var j = 0; j < gDialog.columnsInput.value; j++)
+      {
+        var newCell = gActiveEditor.document.createElement("td");
+        if (aAttributes)
+          for (var k = 0; k < aAttributes.length; k++)
+          {
+            var attr = aAttributes[k];
+            newCell.setAttribute(attr.name, attr.value);
+          }
+        if (newCell)
+        {
+          newRow.appendChild(newCell);
+        }
+      }
+    }
+  }
+}
+
 function onAccept()
 {
   var wrapping = gDialog.textWrapping.selectedItem.value;
@@ -90,37 +118,128 @@ function onAccept()
   var cellSpacing = gDialog.cellSpacing.value;
   var cellPadding = gDialog.cellPadding.value;
 
-  var rows = 
+  var useCSS = CssUtils.getUseCSSPref();
+
   gActiveEditor.beginTransaction();
 
-  var tableElement = gActiveEditor.createElementWithDefaults("table");
-  var tableBody = gActiveEditor.createElementWithDefaults("tbody");
+  var tableElement = gActiveEditor.document.createElement("table");
+  var tableBody = gActiveEditor.document.createElement("tbody");
+  
   if (tableBody)
   {
+    
     tableElement.appendChild(tableBody);
 
-    // Create necessary rows and cells for the table
-    for (var i = 0; i < gDialog.rowsInput.value; i++)
+    switch(useCSS)
     {
-      var newRow = gActiveEditor.createElementWithDefaults("tr");
-      if (newRow)
-      {
-        tableBody.appendChild(newRow);
-        for (var j = 0; j < gDialog.columnsInput.value; j++)
+      case 0:
+        tableElement.setAttribute("border", gDialog.borderInput.value);
+        tableElement.setAttribute("width", Number(gDialog.widthInput.value) +
+                                            (gDialog.widthPixelOrPercentMenulist.value == "pixels" ? "" : "%"));
+        tableElement.setAttribute("cellpadding", gDialog.cellSpacing.value);
+        tableElement.setAttribute("cellspacing", gDialog.cellPadding.value);
         {
-          var newCell = gActiveEditor.createElementWithDefaults("td");
-          if (newCell)
-          {
-            newRow.appendChild(newCell);
-          }
+          var attributes = [];
+          if (gDialog.horizAlignment.value)
+            attributes.push( { name: "align",
+                               value: gDialog.horizAlignment.value });
+          if (gDialog.vertAlignment.value)
+            attributes.push( { name: "valign",
+                               value: gDialog.vertAlignment.value });
+          if (gDialog.textWrapping.value)
+            attributes.push( { name: "nowrap",
+                               value: "nowrap" });
         }
-      }
+        CreateRowsAndCells(tableBody, attributes);
+        break;
+
+      case 1:
+        {
+          var styleAttr = "width: " + Number(gDialog.widthInput.value) +
+                            (gDialog.widthPixelOrPercentMenulist.value == "pixels" ? "px" : "%") +
+                            ";";
+          styleAttr += "border-spacing: " + gDialog.cellPadding.value + "px;";
+          styleAttr += "border: outset " + gDialog.borderInput.value + "px;";
+          if (gDialog.horizAlignment.value)
+            styleAttr += "text-align:" + gDialog.horizAlignment.value;
+          tableElement.setAttribute("style", styleAttr);
+
+          styleAttr = "";
+          if (gDialog.vertAlignment.value)
+            styleAttr += "vertical-align: " + gDialog.vertAlignment.value + ";";
+          if (gDialog.textWrapping.value)
+            styleAttr += "white-space: nowrap;";
+          if (gDialog.cellPadding.value)
+            styleAttr += "padding: " + gDialog.cellPadding.value + "px;";
+          if (gDialog.borderInput.value != "0")
+            styleAttr += "border: inset 1px;";
+          if (styleAttr)
+            CreateRowsAndCells(tableBody, [ { name: "style",
+                                              value: styleAttr} ] ); 
+          else
+            CreateRowsAndCells(tableBody, null);
+          
+        }
+        break;
+
+      case 2:
+        {
+          CreateRowsAndCells(tableBody, null);
+          // first, build the selector we're going to use
+          var cssToggler = gDialog.cssToggler;
+          var selectorText = (cssToggler.newID ?   '#' + cssToggler.newID : "") +
+                             (cssToggler.newClass? '.' + cssToggler.newClass : "");
+          var tableProperties = [
+            { priority: false, property: "border",
+              value: "outset " + gDialog.borderInput.value + "px" },
+            { priority: false, property: "border-spacing",
+              value: gDialog.cellPadding.value + "px" },
+            { priority: false, property: "width",
+              value: Number(gDialog.widthInput.value) +
+                       (gDialog.widthPixelOrPercentMenulist.value == "pixels" ? "px" : "%") },
+            { priority: false, property:  "text-align",
+              value: gDialog.horizAlignment.value }
+          ];
+          CssUtils.addRuleForSelector(gActiveEditor.document,
+                                      selectorText,
+                                      tableProperties);
+          if (cssToggler.newID)
+            tableElement.setAttribute("id", cssToggler.newID);
+          if (cssToggler.newClass)
+            tableElement.setAttribute("class", cssToggler.newClass);
+
+          var cellProperties = [];
+          if (gDialog.vertAlignment.value)
+            cellProperties.push( { priority: false,
+                                  property: "vertical-align",
+                                  value: gDialog.vertAlignment.value } );
+
+          if (gDialog.textWrapping.value)
+            cellProperties.push( { priority: false,
+                                  property: "white-space",
+                                  value: "nowrap" } );
+
+          if (gDialog.cellPadding.value)
+            cellProperties.push( { priority: false,
+                                  property: "padding",
+                                  value: gDialog.cellPadding.value + "px" } );
+
+          if (gDialog.borderInput.value != "0")
+            cellProperties.push( { priority: false,
+                                  property: "border",
+                                  value: "inset 1px" } );
+          cellSelectorText = selectorText + " > * > tr > td," +
+                             selectorText + " > * > tr > th";
+          CssUtils.addRuleForSelector(gActiveEditor.document,
+                                      cellSelectorText + "",
+                                      cellProperties);
+        }
+        break;
     }
+
     // true means delete selection when inserting
     gActiveEditor.insertElementAtSelection(tableElement, true);
-    gActiveEditor.setAttribute(tableElement, "border", gDialog.borderInput.value);
-    gActiveEditor.setAttribute(tableElement, "width", Number(gDialog.widthInput.value) +
-                                        (gDialog.widthPixelOrPercentMenulist.value == "pixels" ? "" : "%"));
+
 
   }
   gActiveEditor.endTransaction();
@@ -128,6 +247,9 @@ function onAccept()
 
 function SelectSize(cell)
 {
+  if (document.documentElement.getButton("accept").hasAttribute("disabled"))
+    return;
+
   var columns  = (gCellID % 10);
   var rows     = Math.ceil(gCellID / 10);
 
