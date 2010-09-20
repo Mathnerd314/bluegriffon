d1ddb0793c31940b7bf8d180c640600f9c24a00b
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2010-09-20 14:46:54 +0000

    list-style-type, letter-spacing, word-spacing, word-wrap
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@222 c25cacc5-b657-0410-8d04-e2f7359d2943

--- sidebars/cssproperties/content/cssproperties.js
+++ sidebars/cssproperties/content/cssproperties.js
@@ -713,6 +713,7 @@ function CloseAllSection(aAlsoCloseOriginalTarget)
 #include geometry.js.inc
 #include position.js.inc
 #include borders.js.inc
+#include lists.js.inc
 #include transforms.js.inc
 #include flexbox.js.inc
 #include columns.js.inc
