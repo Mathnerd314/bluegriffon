087ac6c6644bfdebbcb8774e865a4cbb91ea1fca
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2011-05-24 13:30:05 +0000

    fix mozilla-central trunk bustage
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@702 c25cacc5-b657-0410-8d04-e2f7359d2943

--- app/nsEditorApp.cpp
+++ app/nsEditorApp.cpp
@@ -45,13 +45,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#include "plstr.h"
+#include "prprf.h"
+#include "prenv.h"
+
 #include "nsCOMPtr.h"
 #include "nsILocalFile.h"
 #include "nsStringGlue.h"
 
 #ifdef XP_WIN
-// we want a wmain entry point
+// we want to use the DLL blocklist if possible
 #define XRE_WANT_DLL_BLOCKLIST
+// we want a wmain entry point
 #include "nsWindowsWMain.cpp"
 #endif
 
@@ -71,6 +76,25 @@ static void Output(const char *fmt, ... )
   va_end(ap);
 }
 
+/**
+ * Return true if |arg| matches the given argument name.
+ */
+static PRBool IsArg(const char* arg, const char* s)
+{
+  if (*arg == '-')
+  {
+    if (*++arg == '-')
+      ++arg;
+    return !PL_strcasecmp(arg, s);
+  }
+
+#if defined(XP_WIN) || defined(XP_OS2)
+  if (*arg == '/')
+    return !PL_strcasecmp(++arg, s);
+#endif
+
+  return PR_FALSE;
+}
 
 /**
  * A helper class which calls NS_LogInit/NS_LogTerm in its scope.
@@ -94,6 +118,36 @@ int main(int argc, char* argv[])
   }
   appini->SetNativeLeafName(NS_LITERAL_CSTRING("application.ini"));
 
+  // Allow firefox.exe to launch XULRunner apps via -app <application.ini>
+  // Note that -app must be the *first* argument.
+  char *appEnv = nsnull;
+  const char *appDataFile = PR_GetEnv("XUL_APP_FILE");
+  if (appDataFile && *appDataFile) {
+    rv = XRE_GetFileFromPath(appDataFile, getter_AddRefs(appini));
+    if (NS_FAILED(rv)) {
+      Output("Invalid path found: '%s'", appDataFile);
+      return 255;
+    }
+  }
+  else if (argc > 1 && IsArg(argv[1], "app")) {
+    if (argc == 2) {
+      Output("Incorrect number of arguments passed to -app");
+      return 255;
+    }
+
+    rv = XRE_GetFileFromPath(argv[2], getter_AddRefs(appini));
+    if (NS_FAILED(rv)) {
+      Output("application.ini path not recognized: '%s'", argv[2]);
+      return 255;
+    }
+
+    appEnv = PR_smprintf("XUL_APP_FILE=%s", argv[2]);
+    PR_SetEnv(appEnv);
+    argv[2] = argv[0];
+    argv += 2;
+    argc -= 2;
+  }
+
   nsXREAppData *appData;
   rv = XRE_CreateAppData(appini, &appData);
   if (NS_FAILED(rv)) {
@@ -103,5 +157,7 @@ int main(int argc, char* argv[])
 
   int result = XRE_main(argc, argv, appData);
   XRE_FreeAppData(appData);
+  if (appEnv)
+    PR_smprintf_free(appEnv);
   return result;
 }
