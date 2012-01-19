5b3ce845f09671acdddf91b5e66ee211ba3f1d08
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2012-01-19 14:12:02 +0000

    updating gecko
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@933 c25cacc5-b657-0410-8d04-e2f7359d2943

--- langpacks/getlangpacks.sh
+++ langpacks/getlangpacks.sh
@@ -3,18 +3,26 @@
 VERSION=$1
 BG_VERSION=$2
 
+LOCALES="cs de es-ES fi fr he hu it ja ko nl pl sl sv-SE zh-CN zh-TW"
+
 R_VERSION=`echo $VERSION | sed -e "s/\./\\\\\./" | sed -e "s/\n//"`
 R_BG_VERSION=`echo $BG_VERSION | sed -e "s/\./\\\\\./" | sed -e "s/\n//"`
 
-echo $R_VERSION
-for i in `ls -1 | grep -v Makefile`;
+wget ftp://ftp.mozilla.org/pub/BlueGriffon/nightly/latest-mozilla-central/BlueGriffon-$VERSION.en-US.langpack.xpi
+for i in `echo $LOCALES`
 do
-    wget ftp://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central-l10n/win32/xpi/firefox-$VERSION.$i.langpack.xpi
-    mkdir /tmp/$i
+    wget ftp://ftp.mozilla.org/pub/BlueGriffon/nightly/latest-mozilla-central-l10n/win32/xpi/BlueGriffon-$VERSION.$i.langpack.xpi
+done
+
+for i in `echo "en-US "$LOCALES`
+do
     P=`pwd`
-    cd /tmp/$i
-    unzip $P/firefox-$VERSION.$i.langpack.xpi
-    cat install.rdf | sed -e "s/firefox.mozilla.org/bluegriffon.org/" \
+    cd /tmp
+    rm -fr $i
+    mkdir $i
+    cd $i
+    unzip $P/BlueGriffon-$VERSION.$i.langpack.xpi
+    cat install.rdf | sed -e "s/BlueGriffon.mozilla.org/bluegriffon.org/" \
                     | sed -e "s/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/bluegriffon@disruptive-innovations.com/" \
                     | sed -e "s/$R_VERSION/$R_BG_VERSION/" \
                     > foo
@@ -32,5 +40,11 @@ locale tipoftheday "$i" extensions/tipoftheday/
 " >> foo
     cat foo | grep -v "manifest" > chrome.manifest
     rm foo
+    cd ..
+    cp -r $i $P
     cd $P
+    rm BlueGriffon-$VERSION.$i.langpack.xpi
 done
+
+find . -name \*.dtd -exec svn add {} \;
+find . -name \*.properties -exec svn add {} \;
