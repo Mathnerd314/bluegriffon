b8b19892e8d6930b5ea998b5944ba1a27f31a70c
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2013-03-28 17:07:11 +0000

    fixing build system bustage
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@1159 c25cacc5-b657-0410-8d04-e2f7359d2943

--- build.mk
+++ build.mk
@@ -1,58 +1,73 @@
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is the Mozilla build system.
-#
-# The Initial Developer of the Original Code is
-# the Mozilla Foundation <http://www.mozilla.org/>.
-# Portions created by the Initial Developer are Copyright (C) 2006
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#   Benjamin Smedberg <benjamin@smedbergs.us> (Initial Code)
-#
-# Alternatively, the contents of this file may be used under the terms of
-# either the GNU General Public License Version 2 or later (the "GPL"), or
-# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-# in which case the provisions of the GPL or the LGPL are applicable instead
-# of those above. If you wish to allow use of your version of this file only
-# under the terms of either the GPL or the LGPL, and not to allow others to
-# use your version of this file under the terms of the MPL, indicate your
-# decision by deleting the provisions above and replace them with the notice
-# and other provisions required by the GPL or the LGPL. If you do not delete
-# the provisions above, a recipient may use your version of this file under
-# the terms of any one of the MPL, the GPL or the LGPL.
-#
-# ***** END LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifndef LIBXUL_SDK
-#include $(topsrcdir)/toolkit/toolkit-tiers.mk
+################################################
+# Parallel build on Windows with GNU make check
+
+default::
+ifeq (,$(findstring pymake,$(MAKE)))
+ifeq ($(HOST_OS_ARCH),WINNT)
+ifneq (1,$(NUMBER_OF_PROCESSORS))
+	@echo $(if $(findstring -j,$(value MAKEFLAGS)), \
+$(error You are using GNU make to build Firefox with -jN on Windows. \
+This will randomly deadlock. To compile a parallel build on Windows \
+run "python -OO build/pymake/make.py -f client.mk build". \
+See https://developer.mozilla.org/en/pymake for more details.))
+endif
+endif
 endif
 
-#TIERS += app
-
 installer:
-	@echo "BlueGriffon doesn't have an installer yet."
-	@exit 1
+	@$(MAKE) -C bluegriffon/installer installer
 
 package:
 	@$(MAKE) -C bluegriffon/installer
 
-install::
-	@echo "BlueGriffon can't be installed directly."
-	@exit 1
+package-compare:
+	@$(MAKE) -C bluegriffon/installer package-compare
 
-distribution::
-ifdef MOZ_UPDATE_PACKAGING
-	$(MAKE) -C $(DIST)/../tools/update-packaging
+stage-package:
+	@$(MAKE) -C bluegriffon/installer stage-package
+
+install::
+	@$(MAKE) -C bluegriffon/installer install
+
+clean::
+	@$(MAKE) -C bluegriffon/installer clean
+
+distclean::
+	@$(MAKE) -C bluegriffon/installer distclean
+
+source-package::
+	@$(MAKE) -C bluegriffon/installer source-package
+
+upload::
+	@$(MAKE) -C bluegriffon/installer upload
+
+source-upload::
+	@$(MAKE) -C bluegriffon/installer source-upload
+
+hg-bundle::
+	@$(MAKE) -C bluegriffon/installer hg-bundle
+
+l10n-check::
+	@$(MAKE) -C bluegriffon/locales l10n-check
+
+ifdef ENABLE_TESTS
+# Implemented in testing/testsuite-targets.mk
+
+mochitest-bluegriffon-chrome:
+	$(RUN_MOCHITEST) --bluegriffon-chrome
+	$(CHECK_TEST_ERROR)
+
+mochitest:: mochitest-bluegriffon-chrome
+
+.PHONY: mochitest-bluegriffon-chrome
+
+mochitest-metro-chrome:
+	$(RUN_MOCHITEST) --metro-immersive --bluegriffon-chrome
+	$(CHECK_TEST_ERROR)
+
+
 endif
