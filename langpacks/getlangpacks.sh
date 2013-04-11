4a476fc4e8a6acacaa5d4668e6a6c6e4c57e1099
dglazman <dglazman@c25cacc5-b657-0410-8d04-e2f7359d2943>
2013-04-11 13:00:16 +0000

    move to new build system and new langpack organization
    git-svn-id: http://sources.disruptive-innovations.com/bluegriffon/trunk@1164 c25cacc5-b657-0410-8d04-e2f7359d2943

--- langpacks/getlangpacks.sh
+++ langpacks/getlangpacks.sh
@@ -6,9 +6,10 @@ BG_VERSION=$2
 LOCALES="cs de es-ES fi fr gl he hu it ja ko nl pl sl sr sv-SE zh-CN zh-TW"
 #LOCALES="gl"
 
-R_VERSION=`echo $VERSION | sed -e "s/\./\\\\\./" | sed -e "s/\n//"`
-R_BG_VERSION=`echo $BG_VERSION | sed -e "s/\./\\\\\./" | sed -e "s/\n//"`
+R_VERSION=`echo $VERSION | sed -e "s/\./\\\\\./g" | sed -e "s/\n//"`
+R_BG_VERSION=`echo $BG_VERSION | sed -e "s/\./\\\\\./g" | sed -e "s/\n//"`
 
+echo $R_VERSION " " $R_BG_VERSION
 wget ftp://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-$VERSION.en-US.langpack.xpi
 for i in `echo $LOCALES`
 do
@@ -23,12 +24,16 @@ do
     mkdir $i
     cd $i
     unzip $P/firefox-$VERSION.$i.langpack.xpi
-    cat install.rdf | sed -e "s/firefox.mozilla.org/bluegriffon.org/" \
-                    | sed -e "s/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/bluegriffon@disruptive-innovations.com/" \
-                    | sed -e "s/$R_VERSION/$R_BG_VERSION/" \
-                    > foo
-    mv foo install.rdf
-    cp chrome.manifest foo
+    for j in install.rdf chrome.manifest
+    do
+    	cat $j | sed -e "s/firefox.mozilla.org/bluegriffon.org/" \
+    	       | sed -e "s/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/bluegriffon@disruptive-innovations.com/" \
+    	       | sed -e "s/$R_VERSION/$R_BG_VERSION/" \
+             > foo
+      mv foo $j
+    done
+    echo "manifest bluegriffon/chrome.manifest application=bluegriffon@disruptive-innovations.com" >> chrome.manifest
+    mkdir bluegriffon
     echo "locale bluegriffon "$i" base/locale/bluegriffon/
 locale branding "$i" base/locale/branding/
 locale fs "$i" extensions/fs/
@@ -38,14 +43,14 @@ locale domexplorer "$i" sidebars/domexplorer/
 locale scripteditor "$i" sidebars/scripteditor/
 locale stylesheets "$i" sidebars/stylesheets/
 locale tipoftheday "$i" extensions/tipoftheday/
-" >> foo
-    cat foo | grep -v "manifest" > chrome.manifest
-    rm foo
+" > bluegriffon/chrome.manifest
+    rm -f foo
     cd ..
     cp -r $i $P
     cd $P
-    rm firefox-$VERSION.$i.langpack.xpi
+#    rm firefox-$VERSION.$i.langpack.xpi
 done
 
 find . -name \*.dtd -exec svn add {} \;
 find . -name \*.properties -exec svn add {} \;
+find . -name \*.manifest -exec svn add {} \;
